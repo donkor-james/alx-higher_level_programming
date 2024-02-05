a19f50877355f6da39979f0799ce0f36c833f5c9
Donkor James <jamesdonkor987@gmail.com>
2024-02-05 20:55:13 +0000

    lookup task and readme file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,3 +58,10 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
+
+
+# name = "James"
+
+# # name.capitalize
+
+# print(name)
