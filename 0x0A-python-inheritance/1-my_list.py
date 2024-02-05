2205d11c6c4893ad82d7bf8e8ca6e8eed7258ab9
Donkor James <jamesdonkor987@gmail.com>
2024-02-05 22:49:58 +0000

    created 1-my_list.py and tests/1-my_list.txt

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+
+
+class MyList:
+    """a subclass of list"""
+
+    def __init__(self):
+        """initializes the object"""
+        super().__init__(self)
+
+
+def print_sorted(self):
+    """prints sorted list"""
+    print(sorted(self))
