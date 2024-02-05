c89faaa3a80f9e9ff9621bd3bee086d84602c975
Donkor James <jamesdonkor987@gmail.com>
2024-02-05 22:58:45 +0000

    all tasks

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,13 +4,12 @@ contains the MyList class
 """
 
 
-class MyList:
+class MyList(list):
     """a subclass of list"""
-
     def __init__(self):
         """initializes the object"""
-        super().__init__(self)
+        super().__init__()
 
     def print_sorted(self):
-        """prints sorted list"""
+        """prints the sorted list"""
         print(sorted(self))
