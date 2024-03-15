48f6d6ac8aec2d66630da3d2e946ffbc2100545c
Donkor James <jamesdonkor987@gmail.com>
2024-03-15 02:42:08 +0000

    sql

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a JSON file-reading function."""
+import sys
 import json
 
 
@@ -7,3 +8,12 @@ def load_from_json_file(filename):
     """Create a Python object from a JSON file."""
     with open(filename) as f:
         return json.load(f)
+
+
+save = __import__('5-save_to_json_file').save_to_json_file
+
+items = load_from_json_file()
+
+my_list = []
+
+my_list.extend(sys.argv[1:])
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,3 +5,14 @@
 def class_to_json(obj):
     """Return the dictionary represntation of a simple data structure."""
     return obj.__dict__
+
+
+class Students:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        return self.__dict__
