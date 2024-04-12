0e73db336845e047cf684592d4fbd573be0ec0e3
Donkor James <jamesdonkor987@gmail.com>
2024-04-12 00:29:26 +0000

    updated

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 const list = require("./100-data.js").list;
-const new_list = [];
 console.log(list);
-list.map((item, index) => (new_list[index] = item * index));
-console.log(new_list);
+console.log(list.map((item, index) => item * index));
