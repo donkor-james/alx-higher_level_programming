94e2361b5266e36d2775b7f068a14e6f90996cbe
James Donkor <jamesdonkor987@gmail.com>
2024-04-25 15:05:58 -0700

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,21 +4,40 @@ import MySQLdb
 import sys
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=db_password, db=database, charset="utf8")
-cur = conn.cursor()
+def list_states (username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+    Ags:
+        username: mysql username
+        password: mysql password
+        database: mysql database
+    """
+    # Connect to the MySQL server
+    conn = MySQLdb.connect(host='localhost',\
+            port=3306,\
+            user=username,\
+            passwd=password,\
+            db=database)
+    cursor = conn.cursor()
 
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    # Execute the SQL query to fetch all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-rows = cur.fetchall()
+    # Fetch all the rows from the query result
+    rows = cursor.fetchall()
 
-for row in rows:
-	print(row)
+    # Print the results
+    for row in rows:
+        print(row)
 
-conn.close()
+    # Close the database connection
+    conn.close()
 
-
+# Example usage
 if __name__ == '__main__':
-	username = sys.arg[1]
-	db_password = sys.arg[2]
-	database = sys.arg[3]
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
 
