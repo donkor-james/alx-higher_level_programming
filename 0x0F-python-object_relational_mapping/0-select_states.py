389c7dd7f55cfb51750f782ba0e0d7880764d47b
James Donkor <jamesdonkor987@gmail.com>
2024-04-25 14:26:53 -0700

    readme file and 0-select_states file created

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""scrpit that list all states from hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=db_password, db=database, charset="utf8")
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM states ORDER BY states.id DESC")
+
+rows = cur.fetchall()
+
+for row in rows:
+	print(row)
+
+conn.close()
+
+
+if __name__ == '__main__':
+	username = sys.arg[1]
+	db_password = sys.arg[2]
+	database = sys.arg[3]
+
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+#README file for python_object_relational_mapping
