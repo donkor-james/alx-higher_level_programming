c8ebd6701ed19078fcdd554d47d0366f3f1fba75
Donkor James <jamesdonkor987@gmail.com>
2024-05-01 00:03:21 +0000

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that print send request and display the body size of the response
-curl -sl "$1" | grep -i "Content-Length" | awk '{print $2}'
+curl -sl "$1" | grep -i Content-Length | awk '{print $2}'
