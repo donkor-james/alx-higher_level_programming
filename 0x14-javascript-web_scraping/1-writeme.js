98e43edcf6d02c5cfd9acf060d93667f904dd7d1
James Donkor <jamesdonkor987@gmail.com>
2024-05-25 10:45:44 -0700

    updated

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,4 +3,5 @@ const fs = require("fs");
 // import the buit in module fs
 fs.writeFile(process.argv[2], process.argv[3], (error) => {
   if (error) console.log(error);
+// if statement to check for error condition
 });
